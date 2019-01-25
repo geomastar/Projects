fa572200fd4a8edab29cd08e93c297665f1e86b3
geomastar <46405952+geomastar@users.noreply.github.com>
2019-01-25 14:49:37 +0000

    insertion sort

---
+++ insertionSort1/insertionSort1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace insertionSort1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int[] arr = new int[10];
+
+            for(int i = 0; i < arr.Length; i ++)
+            {
+                arr[i] = rnd.Next(1, 50);
+            }
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                int nextItem = arr[i];
+                int j = i - 1;
+
+                while (j >= 0 && nextItem < arr[j])
+                {
+                    arr[j + 1] = arr[j];
+                    j = j - 1;
+                }
+
+                arr[j + 1] = nextItem;
+            }
+
+            foreach (int item in arr)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
