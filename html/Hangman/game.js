d0b7ce6c4425641ae5887ecae0276ca79e622c31
geomastar <46405952+geomastar@users.noreply.github.com>
2019-02-11 11:14:36 +0000

    stuff

---
+++ html/Hangman/game.js
@@ -0,0 +1,32 @@
+const letter0Span = getElementById("letter0")
+const letter1Span = getElementById("letter1")
+const letter2Span = getElementById("letter2")
+const letter3Span = getElementById("letter3")
+const letter4Span = getElementById("letter4")
+const letter5Span = getElementById("letter5")
+const letter6Span = getElementById("letter6")
+const letter7Span = getElementById("letter7")
+const letter8Span = getElementById("letter8")
+const letter9Span = getElementById("letter9")
+const letter10Span = getElementById("letter10")
+const letter11Span = getElementById("letter11")
+const letterSpans = 
+[
+	letter0Span, letter1Span, letter2Span,
+	letter3Span, letter4Span, letter5Span,
+	letter6Span, letter7Span, letter8Span,
+	letter9Span, letter10Span, letter11Span
+];
+
+function createLetter(theLetter)
+{
+	var letter = theLetter;	
+	var guessed = 0;
+	
+	function constructor()
+	{
+		const 
+	}
+	
+	return 
+}
