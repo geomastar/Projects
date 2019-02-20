bbf44589732456a55de1dc68f0b1b24fa5078331
geomastar <46405952+geomastar@users.noreply.github.com>
2019-02-20 20:48:34 +0000

    progress

--- html/Hangman/game.js
+++ html/Hangman/game.js
@@ -1,15 +1,15 @@
-const letter0Span = document.getElementById("letter0")
-const letter1Span = document.getElementById("letter1")
-const letter2Span = document.getElementById("letter2")
-const letter3Span = document.getElementById("letter3")
-const letter4Span = document.getElementById("letter4")
-const letter5Span = document.getElementById("letter5")
-const letter6Span = document.getElementById("letter6")
-const letter7Span = document.getElementById("letter7")
-const letter8Span = document.getElementById("letter8")
-const letter9Span = document.getElementById("letter9")
-const letter10Span = document.getElementById("letter10")
-const letter11Span = document.getElementById("letter11")
+const letter0Span = document.getElementById("letter0");
+const letter1Span = document.getElementById("letter1");
+const letter2Span = document.getElementById("letter2");
+const letter3Span = document.getElementById("letter3");
+const letter4Span = document.getElementById("letter4");
+const letter5Span = document.getElementById("letter5");
+const letter6Span = document.getElementById("letter6");
+const letter7Span = document.getElementById("letter7");
+const letter8Span = document.getElementById("letter8");
+const letter9Span = document.getElementById("letter9");
+const letter10Span = document.getElementById("letter10");
+const letter11Span = document.getElementById("letter11");
 const letterSpans = 
 [
 	letter0Span, letter1Span, letter2Span,
@@ -17,17 +17,49 @@ const letterSpans =
 	letter6Span, letter7Span, letter8Span,
 	letter9Span, letter10Span, letter11Span
 ];
-const dictionary = createDictionary();
+var dictionary = [];
 var wrongGuesses = 0;
+var wordString = "";
+var word = [];
 
-function readTextFile(file)
-{
-	
+function main(array)
+{	
+	dictionary = array;
+	createWord(generateRandomWord());
 }
 
-function createDictionary()
+function playGame(file)
 {
-	readTextFile("Resources\dictionary.txt")
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function()
+	{
+		if (this.readyState == 4 && this.status == 200)
+		{
+			main(this.responseText.split(","));
+		}
+	};
+	xhttp.open("GET", file, true);
+	xhttp.send();
+}
+
+function generateRandomWord()
+{
+	while(true)
+	{
+		var temp = dictionary[Math.floor(Math.random() * dictionary.length)];
+		if (temp.length <= 12)
+		{
+			return temp;
+		}
+	}
+}
+
+function createWord(theWord)
+{
+	for (let i = 0; i < theWord.length; i++)
+	{
+		word.push(createLetter(theWord[i]));
+	}
 }
 
 function createLetter(theLetter)
@@ -39,9 +71,4 @@ function createLetter(theLetter)
 	return letterStruct;
 }
 
-function main()
-{
-	console.log(createLetter('I')[1])
-}
-
-main();
+playGame("Resources/dictionary.txt")
