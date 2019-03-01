7fc1baa44f0ab1ff54d438a4a5d2bd732fa21923
geomastar <46405952+geomastar@users.noreply.github.com>
2019-03-01 13:40:04 +0000

    wot

---
+++ busTimetable/busTimetable/timetable.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace busTimetable
+{
+    class timetable
+    {
+        StreamReader sr;
+
+        struct routeStruct
+        {
+            private string startPoint;
+            private string destination;
+            private List<int> times;
+            
+            public routeStruct(string newStartPoint, string newDestination, List<int> newTimes)
+            {
+                startPoint = newStartPoint;
+                destination = newDestination;
+                times = newTimes;
+            }
+        }
+
+        public timetable(string path)
+        {
+            string newLine;
+            string[] tempLineArray;
+            string tempStartPoint;
+            string tempDestination;
+            List<int> tempTimes;
+
+            sr = new StreamReader(path);        
+            while(sr.Peek() > 0)
+            {
+                newLine = sr.ReadLine();
+                tempLineArray = newLine.Split(' ');
+                tempStartPoint = tempLineArray[0];
+
+            }
+        }
+    }
+}
