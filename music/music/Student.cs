8ecd9ee09f327a066e1116c80cbe6929f855ca99
geomastar <46405952+geomastar@users.noreply.github.com>
2019-03-20 09:59:12 +0000

    u gey

---
+++ music/music/Student.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace music
+{
+    class Student
+    {
+        string firstName;
+        string lastName;
+        int age;
+        string username;
+        string password;
+        Playlist musicCollection;
+
+        public Student(string newFirstName, string newLastName, int newAge, string newPassword, string newPasswordReentered)
+        {
+            firstName = newFirstName;
+            lastName = newLastName;
+            age = newAge;
+
+            username = createUsername(newFirstName, newLastName, newAge);
+
+            musicCollection = new Playlist(); 
+        }
+
+        private string createUsername(string theFirstName, string theLastName, int theAge)
+        {
+            string theUserName;
+            theUserName = theFirstName[0] + theLastName[0] + theAge.ToString();
+            return theUserName;
+        }
+
+        public bool checkPassword(string thePassword)
+        {
+            if (password == thePassword)
+            {
+                return true;
+            }
+            else { return false; }
+        }
+    }
+}
---
+++ music/music/Track.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace music
+{
+    class Track
+    {
+        string title;
+        double length;
+        string artist;
+        string album;
+
+        public Track(string theTitle, double theLength, string theArtist, string theAlbum)
+        {
+            title = theTitle;
+            length = theLength;
+            artist = theArtist;
+            album = theAlbum;
+        }
+
+        #region getters
+        public string getTitle()
+        {
+            return title;
+        }
+        public double getLength()
+        {
+            return length;
+        }
+        public string getArtist()
+        {
+            return artist;
+        }
+        public string getAlbum()
+        {
+            return album;
+        }
+        #endregion getters
+
+    }
+}
