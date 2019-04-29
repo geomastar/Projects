8c81e93964a85920ad4bef196c72e73e25795075
geomastar <46405952+geomastar@users.noreply.github.com>
2019-04-29 23:41:23 +0100

    yor nan

--- deletepls/deletepls/MainWindow.xaml.cs
+++ deletepls/deletepls/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace deletepls
 {
@@ -20,14 +21,38 @@ namespace deletepls
     /// </summary>
     public partial class MainWindow : Window
     {
+        public int g = 10;
+        public double x = 0;
+        public double y = 0;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Tick += timer_Tick;
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 16);
+            timer.Start();
         }
 
-        public double function(double x)
+        private void timer_Tick(object sender, EventArgs e)
         {
-            double y = (Math.Pow(x, 2)*-1) + 4;
+            x += 5;
+            y = function(x, 70, 45);
+
+            label1.Content = x.ToString() + ", " + y.ToString();
+
+            Canvas.SetLeft(jim, x);
+            Canvas.SetBottom(jim, y);
+        }
+
+        public double function(double x, double u, double angle)
+        {
+            double radians = (Math.PI/180)*angle;
+
+            double y = x * Math.Tan(radians) - ((g * Math.Pow(x, 2)) / (2 * Math.Pow(u, 2) * Math.Pow(Math.Cos(radians), 2)));
+
+            return y;
         }
     }
 }
