9b98da68a660cf5066b2a9048ced11ea7041db5a
geomastar <46405952+geomastar@users.noreply.github.com>
2019-05-07 01:32:06 +0100

    u gey nig

---
+++ dynamic surface/dynamic surface/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace dynamic_surface
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Line theLine;
+
+        public MainWindow()
+        {            
+            InitializeComponent();
+
+            theLine = new Line();
+
+            theLine.X1 = 10;
+            theLine.Y1 = 10;
+            theLine.X2 = 50;
+            theLine.Y2 = 10;
+
+            theCanvas.Children.Add(theLine);
+        }
+    }
+}
