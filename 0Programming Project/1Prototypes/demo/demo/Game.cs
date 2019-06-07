10ca61f4e3da760e9fcf0bf874fbd7e1ba9d72d9
geomastar <46405952+geomastar@users.noreply.github.com>
2019-06-07 14:55:56 +0100

    g

---
+++ 0Programming Project/1Prototypes/demo/demo/Game.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace demo
+{
+    class Game
+    {
+        private Map game_Map;
+        private Tank[] game_CurrentTanksArray;
+        private Projectile game_CurrentProjectile;
+
+        public Game()
+        {
+            CompositionTarget.Rendering += UpdateEvent;
+        }
+
+        private void UpdateEvent(object Sender, EventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ 0Programming Project/1Prototypes/demo/demo/Map.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace demo
+{
+    class Map
+    {
+        private Path map_Path;
+        private PathGeometry map_PathGeometry;
+        private PathFigureCollection map_PathFigureCollection;
+        private PathFigure map_PathFigure;
+        private PathSegmentCollection map_PathSegmentCollection;
+        private PolyBezierSegment map_PolyBezierSegment;
+        private PointCollection map_PointCollection;
+
+        public Map()
+        {
+
+        }
+    }
+}
