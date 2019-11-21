302647b1264e160a422c9de01fd059205ce04104
geomastar <46405952+geomastar@users.noreply.github.com>
2019-11-21 21:41:30 +0000

    g

--- 0Programming Project/Metal Lynch (v2.0)/Metal Lynch (v2.0)/Game.cs
+++ 0Programming Project/Metal Lynch (v2.0)/Metal Lynch (v2.0)/Game.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace Metal_Lynch__v2._0_
@@ -10,6 +11,10 @@ namespace Metal_Lynch__v2._0_
     public abstract class Game
     {
         protected Framework game_Framework;
+
+        protected Grid game_Grid;
+        protected RowDefinition game_MainRow;
+        protected RowDefinition game_GUIRow;
 
         protected Canvas game_MainCanvas;
         protected Canvas game_GUICanvas;
@@ -47,6 +52,28 @@ namespace Metal_Lynch__v2._0_
                 Height = 240,
                 Width = 1280
             };
+
+            game_Grid = new Grid()
+            {
+                Height = 690,
+                Width = 1280
+            };
+
+            game_MainRow = new RowDefinition() { Height = new GridLength(450) };
+            game_GUIRow = new RowDefinition() { Height = new GridLength(240) };
+
+            game_Grid.RowDefinitions.Add(game_MainRow);
+            game_Grid.RowDefinitions.Add(game_GUIRow);
+
+            Grid.SetRow(game_MainCanvas, 0);
+            Grid.SetRow(game_GUICanvas, 1);
+
+            game_Grid.Children.Add(game_MainCanvas);
+            game_Grid.Children.Add(game_GUICanvas);
+
+            game_Map = new Map(this);
+
+            game_Projectile = new Projectile(this);
         }
 
         public Canvas GetGame_MainCanvas()
